2ebe7872093f54c80f2a444894e10b6e98acc7ac
jasonTelanoff <jason.telanoff@gmail.com>
2020-04-11 13:24:28 -0700

    yes

---
+++ cash_app/lib/homePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("This is a CASH App")),
+      body: Center(
+        child: Text("This is a CASH App"),
+      ),
+    );
+  }
+}
