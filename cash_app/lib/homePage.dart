6d7d67f0123966cab45805639e975b436b5b39b6
jasonTelanoff <jason.telanoff@gmail.com>
2020-04-11 14:18:04 -0700

    share

--- cash_app/lib/homePage.dart
+++ cash_app/lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,10 +15,45 @@ class HomePage extends StatelessWidget {
               Image.asset('assets/images/cash.jpg'),
             ],
           ),
-          Text("\$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴\$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴", textScaleFactor: 2, style: TextStyle(color: Color(0x99000000))),
+          Text(
+              "\$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴\$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴ \$ € £ ¥ лв CHF Kč kr kn ₾ ft kr zł ₽ lei kr ₺ ₴",
+              textScaleFactor: 2,
+              style: TextStyle(color: Color(0x99000000))),
           Center(
-            child: Text("This is a CASH App", textScaleFactor: 5,),
+            child: Text(
+              "This is a CASH App",
+              textScaleFactor: 5,
+            ),
           ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(bottom: 300),
+            child: FlatButton(
+              color: Color(0x20ffffff),
+              onPressed: () {}, 
+              child: Text('tap anywear to by merhc', textScaleFactor: 4,),
+            ),
+          ),
+          Positioned(
+              bottom: 0.0,
+              child: Center(
+                  child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: RaisedButton(
+                  elevation: 200,
+                  shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(200, 100))),
+                  color: Color(0xddffffff),
+                  onPressed: () {
+                    Share.share('CHECK OUT THIS CASH APP!!!! IT\'S NOT OUT YET',
+                        subject: 'It\'s a CASH APP!!!!!!!!!');
+                  },
+                  child: Text("Share"),
+                ),
+              )))
         ],
       ),
     );
